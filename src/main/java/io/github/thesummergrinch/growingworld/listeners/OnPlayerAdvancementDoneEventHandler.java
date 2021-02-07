5f256de24d1ed4984f9cd449a969923a9cb42de1
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-07 17:21:15 +0100

    OnPlayerAdvancementDoneEventHandler makes sure to grow the WorldBorder whenever a player gets an advancement, whether it be unlocking a new crafting recipe, or an actual advancement.

---
+++ src/main/java/io/github/thesummergrinch/growingworld/listeners/OnPlayerAdvancementDoneEventHandler.java
@@ -0,0 +1,16 @@
+package io.github.thesummergrinch.growingworld.listeners;
+
+import io.github.thesummergrinch.growingworld.worldborder.WorldBorderController;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+
+public class OnPlayerAdvancementDoneEventHandler implements Listener {
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerAdvancementDoneEvent(final PlayerAdvancementDoneEvent event) {
+        WorldBorderController.getInstance().growWorldBorder();
+    }
+
+}
