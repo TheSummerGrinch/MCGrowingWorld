b296508494301610c4f45d745098939ae7127116
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-11 12:44:37 +0100

    Update OnPlayerAdvancementDoneEventHandler.java
    Removed advancement type check, and moved it to WorldBorderController#growOnAdvancement.

--- src/main/java/io/github/thesummergrinch/growingworld/listeners/OnPlayerAdvancementDoneEventHandler.java
+++ src/main/java/io/github/thesummergrinch/growingworld/listeners/OnPlayerAdvancementDoneEventHandler.java
@@ -1,6 +1,5 @@
 package io.github.thesummergrinch.growingworld.listeners;
 
-import io.github.thesummergrinch.growingworld.config.Settings;
 import io.github.thesummergrinch.growingworld.worldborder.WorldBorderController;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,11 +10,7 @@ public class OnPlayerAdvancementDoneEventHandler implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerAdvancementDoneEvent(final PlayerAdvancementDoneEvent event) {
-        if (!event.getAdvancement().getKey().getKey().substring(0, 8)
-                .equals("recipes/") || Boolean.parseBoolean(Settings.getInstance()
-                .getSetting("allow-recipe-advancements"))) {
-            WorldBorderController.getInstance().growWorldBorder();
-        }
+        WorldBorderController.getInstance().growOnAdvancement(event.getAdvancement());
     }
 
 }
